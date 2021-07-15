83ed177907e1b7c843af944b5703ed14d26acf05
Akshay Trikha <akshaytrikha@gmail.com>
2021-07-14 22:04:51 -0700

    Updates

---
+++ precache-manifest.36f524c320613e6165d84b337f51b566.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4f66b7da231a7ad552b4038624ff67",
+    "url": "/Style-Transfer/index.html"
+  },
+  {
+    "revision": "311827ce70c00f5cc975",
+    "url": "/Style-Transfer/static/css/main.06dedd13.chunk.css"
+  },
+  {
+    "revision": "ca2a96457c7474606d0f",
+    "url": "/Style-Transfer/static/js/2.b4727405.chunk.js"
+  },
+  {
+    "revision": "6a3f217cdca97ce53e338d179fb23279",
+    "url": "/Style-Transfer/static/js/2.b4727405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311827ce70c00f5cc975",
+    "url": "/Style-Transfer/static/js/main.0e84e8d1.chunk.js"
+  },
+  {
+    "revision": "3faddbc69adfe33cc3c0",
+    "url": "/Style-Transfer/static/js/runtime-main.69121cae.js"
+  },
+  {
+    "revision": "ec43181a9d6976910f860c48aacd0017",
+    "url": "/Style-Transfer/static/media/babur.ec43181a.jpg"
+  },
+  {
+    "revision": "a13dacc5ef30092ce9872c3471cc26c7",
+    "url": "/Style-Transfer/static/media/bricks.a13dacc5.jpg"
+  },
+  {
+    "revision": "f974f02118b71e7a4e47aca8622f8484",
+    "url": "/Style-Transfer/static/media/guernica.f974f021.jpg"
+  },
+  {
+    "revision": "c9e836e15fca759b8a17ee5e5118fe38",
+    "url": "/Style-Transfer/static/media/linkedin.c9e836e1.png"
+  },
+  {
+    "revision": "228c4fc5a3f4d8ebc45b6bb177aa052f",
+    "url": "/Style-Transfer/static/media/mona-lisa.228c4fc5.jpeg"
+  },
+  {
+    "revision": "9d2ee13ab1596bbaa05a408bfbb57766",
+    "url": "/Style-Transfer/static/media/scream.9d2ee13a.jpeg"
+  },
+  {
+    "revision": "46182d6729afbd0490c1342ae72c65ce",
+    "url": "/Style-Transfer/static/media/squares-concentric-circles.46182d67.jpg"
+  },
+  {
+    "revision": "372e5bc438e3e8d0eb52cc6f7ef44760",
+    "url": "/Style-Transfer/static/media/starry-night.372e5bc4.jpeg"
+  },
+  {
+    "revision": "f6c02290f4dce29b8d29a50132fb3c28",
+    "url": "/Style-Transfer/static/media/stripes.f6c02290.jpg"
+  },
+  {
+    "revision": "2443f87f95564e401f3eab938da84a49",
+    "url": "/Style-Transfer/static/media/towers.2443f87f.jpg"
+  },
+  {
+    "revision": "8efdfd56ad24e0232019d381f3bb4e1e",
+    "url": "/Style-Transfer/static/media/twombly.8efdfd56.jpeg"
+  }
+]);
